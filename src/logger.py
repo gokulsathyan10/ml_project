298bafcd815e95a150a5ed8bb1fbc759268c4eda
Gokul Sathyan <gokulsathyan3007@gmail.com>
2026-02-22 00:40:42 +0000

    data transformation module created

--- src/logger.py
+++ src/logger.py
@@ -2,22 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Create logs/ folder in project root (same level as src/)
-LOG_DIR = "logs"
-os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Log file name with timestamp
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
-
-# Logging format = what each log line looks like
-LOG_FORMAT = "[%(asctime)s] %(levelname)s | %(name)s | %(message)s"
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format=LOG_FORMAT,
-    level=logging.INFO
-)
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-# This is what you import everywhere
-logger = logging.getLogger("mlproject")
+
+)
